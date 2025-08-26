b3f5e99c98322d003a1966d91f162531d7b1dfbc
festimbunjaku <festimbu@gmail.com>
2025-08-26 17:28:41 +0200

    Finished: User/register/login/logout/get/update/

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Protected user route
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::put('/user', [UserController::class, 'update']);
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 });
 
 // Public API routes
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-Route::put('/user', [UserController::class, 'update'])
-    ->middleware('auth:sanctum');
